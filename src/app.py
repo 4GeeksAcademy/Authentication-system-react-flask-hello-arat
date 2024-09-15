2d8796e479c76dde3c3c63568b23d83601fdb02a
Anderson Aguilera <124855089+arat-arjat@users.noreply.github.com>
2024-09-15 23:16:09 +0000

    login y private

--- src/app.py
+++ src/app.py
@@ -6,10 +6,15 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -18,6 +23,10 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret" 
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -67,6 +76,29 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
+@app.route('/api/login', methods=['POST'])
+def login(): 
+    body = request.get_json(silent=True)
+    if body is None: 
+        return jsonify({"msg": "el cuerpo esta vacio"}), 400
+    if 'email' not in body: 
+        return jsonify({"msg": "email es requerido"}), 400
+    if 'password' not in body: 
+        return jsonify({"msg": "password es requerido"}), 400
+    user = User.query.filter_by(email=body["email"]).all()
+    if len(user) == 0: 
+        return jsonify({"msg": "user or password invalid"}), 400
+    if user[0].password != body['password']:
+        return jsonify({"msg": "user or password invalid"}), 400
+    access_token = create_access_token(identity=user[0].email)
+    return jsonify({"msg": "ok", "access_token" : access_token}), 200 
+
+@app.route('/api/private', methods=['GET'])
+@jwt_required()
+def private(): 
+    identity = get_jwt_identity()
+    print(identity)
+    return jsonify({"msg": "thi is a provate message"})
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
